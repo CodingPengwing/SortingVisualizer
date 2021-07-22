400595c2f292bc4da5c56eb91286724ec59cc6ee
ilaylow <chuenleylow@gmail.com>
2021-07-22 16:22:18 +1000

    added in complete dropdown

---
+++ src/components/SortingDescriptor.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "../styles/descriptor.module.scss";
+import dropdown_arrow from "../images/down-arrow-blue.png"
+import SlideToggle from "react-slide-toggle";
+
+export default class Description extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            visible: false,
+        }
+        this.boxClicked = this.boxClicked.bind(this);
+    }
+
+    boxClicked(){
+        this.setState({visible: !this.state.visible})
+    }
+
+    render(){
+        return(
+            <div>
+                <SlideToggle duration = {500} collapsed
+                render = {({toggle, setCollapsibleElement, progress}) => (
+                    <div>
+                        <div className = {styles.question}>
+                            <span style = {{fontSize: "210%", color: "white", fontFamily: "Lato", fontWeight: "lighter"}}>{this.props.header}</span>
+                            <img onClick = {toggle} src = {dropdown_arrow} alt = "Dropdown arrow"/>
+                        </div>
+                        <div ref={setCollapsibleElement}>
+                            <div className = {styles.answer}>
+                                <h3>{this.props.description}</h3>
+                            </div>
+                        </div>
+                    </div>
+                )}>
+                </SlideToggle>
+            </div>
+        )
+    }
+}
